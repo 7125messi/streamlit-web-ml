fe1174a5f3e6e6b334c9ae551fcb5a32a835ab5c
ydzhao <zydeve@outlook.com>
2020-07-06 15:18:42 +0800

    Update 009_streamlit_with_scikit-learn.py
    update 009_streamlit_with_scikit

--- 009_streamlit_with_scikit-learn.py
+++ 009_streamlit_with_scikit-learn.py
@@ -1,5 +1,7 @@
 import streamlit as st 
 import numpy as np
+import pandas as pd
+import pymysql
 
 import matplotlib.pyplot as plt
 from sklearn import datasets
@@ -12,30 +14,33 @@ from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.metrics import accuracy_score
 
-st.title('Streamlit Example')
+st.title('Streamlit-ML-Demo')
 
 st.write("""
-# Explore different classifier and datasets
-Which one is the best?
+    * 探索不同数据集以及不同的分类器的效果
 """)
 
 dataset_name = st.sidebar.selectbox(
-    'Select Dataset',
-    ('Iris', 'Breast Cancer', 'Wine')
+    label = '选择数据集',
+    options = ('Iris', 'Breast Cancer', 'Wine'),
+    index = 0
 )
+st.write(f"## {dataset_name} 数据集")
 
-st.write(f"## {dataset_name} Dataset")
-
+# 获取分类器的名字
 classifier_name = st.sidebar.selectbox(
-    'Select classifier',
-    ('KNN', 'SVM', 'Random Forest')
+    label = '选择分类器',
+    options = ('KNN', 'SVM', 'Random Forest'),
+    index = 0
 )
 
-def get_dataset(name):
+# 获得数据集
+@st.cache(persist=True)
+def get_dataset(dataset_name):
     data = None
-    if name == 'Iris':
+    if dataset_name == 'Iris':
         data = datasets.load_iris()
-    elif name == 'Wine':
+    elif dataset_name == 'Wine':
         data = datasets.load_wine()
     else:
         data = datasets.load_breast_cancer()
@@ -44,52 +49,73 @@ def get_dataset(name):
     return X, y
 
 X, y = get_dataset(dataset_name)
-st.write('Shape of dataset:', X.shape)
-st.write('number of classes:', len(np.unique(y)))
+st.write('数据集形状:', X.shape)
+st.write('类别数:', len(np.unique(y)))
 
-def add_parameter_ui(clf_name):
+# 添加UI界面的参数
+def add_parameter_ui(classifier_name):
     params = dict()
-    if clf_name == 'SVM':
-        C = st.sidebar.slider('C', 0.01, 10.0)
+    if classifier_name == 'SVM':
+        C = st.sidebar.slider(
+            label = 'C', 
+            min_value = 0.00, 
+            max_value = 10.0,
+            step = 0.1
+        )
         params['C'] = C
-    elif clf_name == 'KNN':
-        K = st.sidebar.slider('K', 1, 15)
+    elif classifier_name == 'KNN':
+        K = st.sidebar.slider(
+            label = 'K', 
+            min_value = 1, 
+            max_value = 15,
+            step = 1
+        )
         params['K'] = K
     else:
-        max_depth = st.sidebar.slider('max_depth', 2, 15)
+        max_depth = st.sidebar.slider(
+            label = 'max_depth', 
+            min_value = 2, 
+            max_value = 15,
+            step = 1
+        )
         params['max_depth'] = max_depth
-        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+
+        n_estimators = st.sidebar.slider(
+            label = 'n_estimators', 
+            min_value = 1, 
+            max_value = 100,
+            step = 5
+        )
         params['n_estimators'] = n_estimators
     return params
-
 params = add_parameter_ui(classifier_name)
 
-def get_classifier(clf_name, params):
+
+# 获得分类器
+def get_classifier(classifier_name, params):
     clf = None
-    if clf_name == 'SVM':
+    if classifier_name == 'SVM':
         clf = SVC(C=params['C'])
-    elif clf_name == 'KNN':
+    elif classifier_name == 'KNN':
         clf = KNeighborsClassifier(n_neighbors=params['K'])
     else:
-        clf = clf = RandomForestClassifier(n_estimators=params['n_estimators'], 
-            max_depth=params['max_depth'], random_state=1234)
+        clf = RandomForestClassifier(
+            n_estimators=params['n_estimators'], 
+            max_depth=params['max_depth'], 
+            random_state=1234
+        )
     return clf
-
 clf = get_classifier(classifier_name, params)
+
 #### CLASSIFICATION ####
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
-
 acc = accuracy_score(y_test, y_pred)
-
 st.write(f'Classifier = {classifier_name}')
 st.write(f'Accuracy =', acc)
 
 #### PLOT DATASET ####
-# Project the data onto the 2 primary principal components
 pca = PCA(2)
 X_projected = pca.fit_transform(X)
 
@@ -104,6 +130,5 @@ plt.scatter(x1, x2,
 plt.xlabel('Principal Component 1')
 plt.ylabel('Principal Component 2')
 plt.colorbar()
-
 #plt.show()
 st.pyplot()
